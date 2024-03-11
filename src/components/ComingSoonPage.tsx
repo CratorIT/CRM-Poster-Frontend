fc9658448d411a3155a3e8d706c93251c0736925
Tehseem110 <Tehseem110>
2024-03-11 18:17:31 +0530

    fixed build error

--- src/components/ComingSoonPage.tsx
+++ src/components/ComingSoonPage.tsx
@@ -2,7 +2,7 @@ export default function ComingSoonPage({ name }: { name?: string }) {
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
       <h1 className="text-5xl text-white font-bold mb-8 animate-pulse"> {name} Page is Coming Soon</h1>
-      <p className="text-white text-lg mb-8">We're working hard to bring you something amazing. Stay tuned!</p>
+      <p className="text-white text-lg mb-8">We&apos;re working hard to bring you something amazing. Stay tuned!</p>
     </div>
   );
 }
